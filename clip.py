033900b62c4e62b65d8ec17e4fa8dc797d1328dc
SameerShanbhogue <84760553+SameerShanbhogue@users.noreply.github.com>
2024-03-28 19:56:31 +0530

    Update clip.py

--- clip.py
+++ clip.py
@@ -42,7 +42,7 @@ class CLIPLayer(nn.Module):
 class CLIP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.embedding = CLIPEmbedding(49400,768,77)
+        self.embedding = CLIPEmbedding(49408,768,77)
         self.layers = nn.ModuleList([
             CLIPLayer(12,768) for i in range(12)
 
